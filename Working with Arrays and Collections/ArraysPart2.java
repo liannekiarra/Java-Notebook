74390ac94b6ab02ba9d26410b574378af7e411e6
liannekiarra <liannekiarra@gmail.com>
2025-02-11 02:43:39 +0000

    Create ArraysPart2.java

---
+++ Working with Arrays and Collections/ArraysPart2.java
@@ -0,0 +1,29 @@
+public class ArraysPartTwo{
+  public static void main(String[] args){
+    
+    //demonstrating enhanced for loop
+    int[] intArrayforLianne = {1,2,3,4,5};
+    for (element: intArrayforLianne){
+      System.out.println(element + " ");
+
+  }
+
+}
+
+class arrayToStrings{
+  public static void main(String [] args){
+    int[] integerPackage = {1,2,3,4,5,6,23,323,54,2};
+    System.out.println(Arrays.toString(newArray));
+
+    Object[] objectArray = new Object[3];
+    objectArray[0] = "Hello";
+    objectArray[1] = new StringBuilder("World");
+
+
+
+    
+  }
+
+
+}
+
